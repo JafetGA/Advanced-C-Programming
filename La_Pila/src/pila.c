cc2752094dbc4ffbbf730b6b81d9bfda2c80a021
Elí Jafet García Aguilar <jafet.eli.ga@outlook.com>
2023-04-17 22:17:40 -0600

    Stack

--- La_Pila/src/pila.c
+++ La_Pila/src/pila.c
@@ -1,12 +1,7 @@
 #include "../lib/pila.h"
-int prec(char, char);
-
-const int maxLen = 80;
-char *expresion;
 
 int main()
 {
-    expresion = (char *)malloc(maxLen * sizeof(char));
 
     nodo *nombre = NULL;
     nombre = push(nombre, 'T');
@@ -20,17 +15,4 @@ int main()
     PAUSA;
     CLS;
     return 0;
-}
-
-int prec(char op1, char op2)
-{
-    switch (op1)
-    {
-    case '^':
-
-        break;
-
-    default:
-        break;
-    }
 }
