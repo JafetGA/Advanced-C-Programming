b768a861bb2e22b80b1816d32aad6f91c5e9fba4
Elí Jafet Ubuntu <jafet.eli.ga@outlook.com>
2023-05-13 16:20:19 -0600

    simulador de colas

---
+++ Programs/Ejercicio_colas/src/simulador_de_cola.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../lib/cola.h"
+
+// Macros
+
+#define CAJEROS 8
+
+void simularDia(int, int, int*);
+
+typedef struct
+{
+    int timpoAtencion;
+    int tiempoCliente;
+}Cajero;
+
+
+int main(void)
+{
+    
+    return 0;
+}
+
+void simularDia(int tiempoLlegada, int numeroCajeros, int* timepoAtencionCajeros){
+
+}
